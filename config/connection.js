81ed74ba9ad1093e5c22a7d53ec8b1ad700affa5
suganya somu <sweetsugan12@gmail.com>
2020-10-11 23:38:25 -0700

    commented the local db connection

--- config/connection.js
+++ config/connection.js
@@ -11,13 +11,15 @@ if(process.env.JAWSDB_URL){
     database: "todoagain_db"
   });
 };
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Shreya12!@",
-  database: "burgers_db"
-});
+
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Shreya12!@",
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
